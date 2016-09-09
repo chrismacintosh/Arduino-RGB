eee356dda7d11bea82c52c9931e481912622c49e
chrismacintosh <sobe911@icloud.com>
2016-09-08 23:49:50 -0700

    Fixed various things

--- examples/rgbLibExample/rgbLibExample.ino
+++ examples/rgbLibExample/rgbLibExample.ino
@@ -8,18 +8,54 @@ void setup() {
   //Nothing is required in this example.
   //If you decide to use these RGB classes, it's pretty simple.
   //An instance only takes up about 23 bytes of dynamic memory.
+  //Blink doesn't work right yet. oops. currently fixing it.
 }
+
+int example = 0;
+int prev = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rgb.blink(500);//On/Off at 500ms intervals
-  delay(10000);//Wait 10 seconds before next example
-  rgb.custom(45,90,125);//Just a random color.
-  delay(3000);
-  rgb.red();
-  delay(3000);
-  rgb.blue();
-  delay(3000);
-  rgb.green();
-  delay(3000);
+  if(example == 0){
+    rgb.blink(500);
+    unsigned long current = millis();
+    if(current - prev >= 5000){
+      prev = current;
+      example = 1;
+      rgb.off();
+    }
+  }
+  else if(example == 1){
+    int r = random(0,255);
+    int g = random(0,255);
+    int b = random(0,255);
+
+    //Example of custom (values filled with random numbers)
+    rgb.custom(r,g,b);
+    delay(2500);
+    //Random number class method.
+    rgb.ran();
+    delay(2500);
+    rgb.ran();
+    delay(2500);
+    rgb.ran();
+    example = 2;
+  }
+  else if(example == 2){
+    rgb.red();
+    example = 3;
+    delay(2500);
+  }
+  else if(example == 3){
+    rgb.green();
+    example = 4;
+    delay(2500);
+  }
+  else if(example == 4){
+    rgb.blue();
+    example = 0;
+    delay(2500);
+    rgb.off();
+    prev = millis();
+  }
 }
